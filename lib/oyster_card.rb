5c7d06ab174d883e5a5d2f6cf6366ef91a89f5d6
Sophie <sophie.k.mclean@btinternet.com>
2017-02-22 10:42:21 +0000

    Everything up to challenge 13

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,50 +1,56 @@
-class OysterCard 
+class OysterCard
 	MINIMUM_AMOUNT = 1
 	MAXIMUM_BALANCE = 90
-	attr_reader :balance, :entry_station
+	attr_reader :balance, :entry_station, :journey_history
 	# attr_accessor :touch_in, :touch_out
 	def initialize(balance = 0)
 		@balance = balance
 		@touch_in = false
 		@touch_out = false
+		@journey_history = []
 
 	end
 		def top_up(amount)
-			 if max_balance_limit(amount)  
+			 if max_balance_limit(amount)
 			 	fail "Card Limit exceeded"
 			 else
 			 	@balance += amount
 			 end
 		@balance
-		end 
+		end
 
-		def in_journey?	
-			!!@entry_station		
+		def in_journey?
+			!!@entry_station
 			# @touch_in
 		end
 		def touch_in?(entry_station = nil)
 			if sufficiant_amount?
-					@entry_station  = entry_station 					
+					@entry_station  = entry_station
 					@touch_out = false
 					return @touch_in = true
-				
+
 			else
 				raise "Card doesn't have sufficiant money"
 			end
 		end
-		def touch_out?					
+		def touch_out?(exit_station = nil)
 			deduct(MINIMUM_AMOUNT)
 			@touch_in = false
+			# @exit_station = exit_station
+			update_journey_history(exit_station)
 			@entry_station = nil
 			@touch_out = true
 		end
-		
+
+    def update_journey_history(exit_station)
+    @journey_history << {entry_station => exit_station}
+		end
 	private
 
 	def max_balance_limit(amount)
 			@balance + amount > MAXIMUM_BALANCE
 		end
-	
+
 	def min_balance_limit(amount)
 		@balance - amount <= 0
 	end
@@ -52,9 +58,9 @@ class OysterCard
 		@balance >= MINIMUM_AMOUNT
 	end
 
-	def deduct(amount)			
-		if  min_balance_limit(amount) 
-			fail "There is 0 balance on the card" 
+	def deduct(amount)
+		if  min_balance_limit(amount)
+			fail "There is 0 balance on the card"
 		else
 			@balance -= amount
 		end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,33 +1,37 @@
 require 'oyster_card'
 describe OysterCard do
- subject(:card){described_class.new}	
+  before(:each) do
+    card.top_up(OysterCard::MAXIMUM_BALANCE)
+  end
+ subject(:card){described_class.new}
 
 	describe "attributes of OysterCard" do
      it "should check the balance" do
-     	expect(card.balance).to eq(0)
+     	expect(card.balance).to eq(90)
      end
-	end 
+	end
 	describe "top_up of OysterCard" do
-		it{is_expected.to respond_to(:top_up).with(1).argument}
+		# it{is_expected.to respond_to(:top_up).with(1).argument}
 
 		it 'should raise error if balance>90' do
 			error = "Card Limit exceeded"
-			card.top_up(OysterCard::MAXIMUM_BALANCE)
+			#card.top_up(OysterCard::MAXIMUM_BALANCE)
 			# p card.balance.to_s + "Balance"
 			expect{card.top_up(1)}.to raise_error error
-		end 
+		end
 
 		it "should top up the balance" do
+      card = OysterCard.new
 			expect{card.top_up(10)}.to change{card.balance}.by 10
-		end 
-	end 
+		end
+	end
 	describe 'deductions from oyster card' do
-		
+
 		# it {is_expected.to respond_to(:deduct).with(1).argument}
 		#  it 'should raise an error if balance is 0' do
 		#  	card.deduct(card.balance-1)
 		#  	error = "There is 0 balance on the card"
-		#  	expect{card.deduct(1)}.to raise_error error 
+		#  	expect{card.deduct(1)}.to raise_error error
 		#  end
 	end
 
@@ -35,12 +39,13 @@ describe OysterCard do
 		it {should respond_to(:in_journey?)}
 		it {should respond_to(:touch_in?)}
 		it {should respond_to(:touch_out?)}
-		
+
 		it "validates that card has the min_amout for journey" do
+      card = OysterCard.new
 			error = "Card doesn't have sufficiant money"
-			expect{card.touch_in?}.to raise_error error 
+			expect{card.touch_in?}.to raise_error error
 		end
-		it 'validates card was not in journey' do			
+		it 'validates card was not in journey' do
 			expect(card).not_to be_in_journey
 		end
 		# it 'validates card is in journey' do
@@ -48,24 +53,34 @@ describe OysterCard do
 		# 	card.touch_in?
 		# 	expect(card).to be_in_journey
 		# end
-		it 'validates that card is deducted on touch out' do			
-			card.top_up(OysterCard::MAXIMUM_BALANCE)
+		it 'validates that card is deducted on touch out' do
+			#card.top_up(OysterCard::MAXIMUM_BALANCE)
 			card.touch_in?
 			expect{card.touch_out?}.to change{card.balance}.by(-OysterCard::MINIMUM_AMOUNT)
 		end
 
-	end	
+	end
 	describe 'entry station' do
-		let(:entry_station) { double('Aldgate East')}		
-		# let(:entry){ :entry_station = !nil} 
+		let(:entry_station) { double('Aldgate East')}
+		# let(:entry){ :entry_station = !nil}
 		it 'confirms card has an entry station on touch_in' do
-			
-			card.top_up(OysterCard::MINIMUM_AMOUNT)
+
+			#card.top_up(OysterCard::MINIMUM_AMOUNT)
 			card.touch_in?(:entry_station)
 			expect(card.entry_station).to eq :entry_station
 		end
-
-
-
 	end
+
+   describe "save old journeys"do
+     it "checks that touching out save the journey" do
+
+     card.touch_in?("Liverpool")
+      card.touch_out?("Euston")
+      expect(card.journey_history).to eq([{"Liverpool" =>"Euston"}])
+
+     end
+     it "checks that a new card has an empty journey history" do
+       expect(card.journey_history).to be_empty
+   end
+ end
 end
